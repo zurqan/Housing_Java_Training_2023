eebad5069675a9be28d1c7db913b688ecd052b07
malzurqan <zurqan@gmail.com>
2023-07-31 17:27:57 +0300

    Advance Session2

--- src/advance/session2/StudentApplication.java
+++ src/advance/session2/StudentApplication.java
@@ -9,13 +9,13 @@ public class StudentApplication {
 
     public static void main(String[] args) {
 
-        Student[] stds={
-                new Student("Mohammad",90,"a@a.com",40,"1"),
-                new Student("Ahmad",80,"ahmad@a.com",30,"2"),
-                new Student("Mosa",70,"mosa@a.com",25,"3"),
-                new Student("Jawad",75,"jawad@a.com",20,"4"),
-                new Student("Dima",95,"dima@a.com",20,"5"),
-                new Student("Mohammad Hamad",85,"hamad@a.com",27,"6")
+        Student[] stds = {
+                new Student("Mohammad", 90, "a@a.com", 40, "1"),
+                new Student("Ahmad", 80, "ahmad@a.com", 30, "2"),
+                new Student("Mosa", 70, "mosa@a.com", 25, "3"),
+                new Student("Jawad", 75, "jawad@a.com", 20, "4"),
+                new Student("Dima", 95, "dima@a.com", 20, "5"),
+                new Student("Mohammad Hamad", 85, "hamad@a.com", 27, "6")
         };
 
         //1. Array of Names
@@ -40,7 +40,7 @@ public class StudentApplication {
                 .count();
         System.out.println(count);
         //5. Names concatenated by ","
-        String concatNames = Arrays.stream(stds).map(a->a.name).reduce(" ",(acc,e)-> acc+","+e);
+        String concatNames = Arrays.stream(stds).map(a -> a.name).reduce(" ", (acc, e) -> acc + "," + e);
 
         String collectedNames = Arrays.stream(stds).map(a -> a.name).collect(Collectors.joining(",", "{", "}"));
         System.out.println("collectedNames = " + collectedNames);
@@ -55,11 +55,12 @@ public class StudentApplication {
                 .toArray(Student[]::new);
 
         //8. Sort students by name
-Student[] stdsOrderedByName = Arrays.stream(stds)
-        .sorted(Comparator.comparing(Student::name))
-        .toArray(Student[]::new);
+        Student[] stdsOrderedByName = Arrays.stream(stds)
+                .sorted(Comparator.comparing(Student::name))
+                .toArray(Student[]::new);
         System.out.println("Arrays.toString(stdsOrderedByName) = " + Arrays.toString(stdsOrderedByName));
     }
 
-    public static record Student(String name,double grade,String email,int age,String id){}
+    public static record Student(String name, double grade, String email, int age, String id) {
+    }
 }
